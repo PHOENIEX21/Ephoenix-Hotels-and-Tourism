e77c43090cf114b3f7aefdfe7bc175bcf736c861
GTO <adunoyepaul7@gmail.com>
2026-08-30 13:07:38 +0100

    fix: add EPhoenix brand text above social icons in top nav

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -7,6 +7,10 @@ import { AuthControls } from './AuthControls';
 export default function MobileMenu() {
   const [open, setOpen] = useState(false);
   return <>
+    <div className="header-brand-text">
+      <strong>EPhoenix Hotels & Tourism</strong>
+      <p>Uniquely, awesome, hospitality</p>
+    </div>
     <div className="header-socials" aria-label="Social media">
       <a href="https://www.facebook.com/ever.phoenix.2025" target="_blank" rel="noreferrer" aria-label="Facebook"><img src="https://cdn.simpleicons.org/facebook/1877F2" alt="Facebook" /></a>
       <a href="https://www.instagram.com/ephoenixhotel.ng" target="_blank" rel="noreferrer" aria-label="Instagram"><img src="https://cdn.simpleicons.org/instagram/E4405F" alt="Instagram" /></a>
